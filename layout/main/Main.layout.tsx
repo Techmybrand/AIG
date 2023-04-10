93d73c4dd630de17143ecd37abec28415b2a66a5
nano235 <revivaladolor@yahoo.com>
2023-04-10 18:07:10 +0100

    fixed build issue

--- layout/main/Main.layout.tsx
+++ layout/main/Main.layout.tsx
@@ -9,21 +9,6 @@ interface Props {
 }
 
 const Main = ({ children }: Props): ReactElement => {
-  const init = useCallback(async () => {
-    // check if device is mobile
-    const width = window.innerWidth
-    const ismobile: boolean = deviceType() === 'mobile' || width <= 700
-
-    // import animations
-    const App = (await import('@/animations')).App
-
-    // initialize new animation
-    new App({ ismobile })
-  }, [])
-
-  useEffect(() => {
-    init()
-  }, [])
   return (
     <div className={styles.layout} data-scroll-container id="container" data-animation="layout">
       <header className={styles.layout_header}>
